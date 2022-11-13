adbc413db2badcea6416a224d90f729ee90d31c2
Imre Nagi <imre.nagi@gojek.com>
2022-11-13 18:57:33 -0500

    use buffer io to iterate the bit until it found CLRF
    Signed-off-by: Imre Nagi <imre.nagi@gojek.com>

--- request.go
+++ request.go
@@ -3,18 +3,28 @@ package http
 import (
 	"bufio"
 	"fmt"
-	"net/textproto"
 	"strings"
 )
 
 func ReadRequest(buf *bufio.Reader) (*Request, error) {
-	tp := textproto.NewReader(buf)
-	line, err := tp.ReadLine()
-	if err != nil {
-		return nil, err
+	// parsing the first line of http request
+	var fl []byte
+	for {
+		line, more, err := buf.ReadLine()
+		if err != nil {
+			return nil, err
+		}
+		if line == nil && !more {
+			fl = line
+			break
+		}
+		fl = append(fl, line...)
+		if !more {
+			break
+		}
 	}
 
-	sl := strings.Split(line, " ")
+	sl := strings.Split(string(fl), " ")
 	if len(sl) < 3 {
 		return nil, fmt.Errorf("invalid http data")
 	}
