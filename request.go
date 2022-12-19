e92b88e027073d46371fe90146ec0869c9a53891
Imre Nagi <imre.nagi@gojek.com>
2022-12-19 15:55:23 -0500

    read http headers and body by using content-length and return base64 response
    Signed-off-by: Imre Nagi <imre.nagi@gojek.com>

--- request.go
+++ request.go
@@ -2,12 +2,83 @@ package http
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
+	"io"
+	"strconv"
 	"strings"
 )
 
 func ReadRequest(buf *bufio.Reader) (*Request, error) {
 	// parsing the first line of http request
+	fl, err := readLine(buf)
+	if err != nil {
+		return nil, err
+	}
+
+	sl := strings.Split(string(fl), " ")
+	if len(sl) < 3 {
+		return nil, fmt.Errorf("invalid http data")
+	}
+	method, path, protoVersion := sl[0], sl[1], sl[2]
+
+	headers, err := readHeader(buf)
+	if err != nil {
+		return nil, err
+	}
+
+	// get content length
+	length, _ := strconv.Atoi(headers["Content-Length"][0])
+	body := make([]byte, length)
+
+	_, err = io.ReadFull(buf, body)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Request{
+		Method:  method,
+		URI:     path,
+		Proto:   protoVersion,
+		Headers: headers,
+		Body:    io.NopCloser(bytes.NewReader(body)),
+	}, nil
+}
+
+func readHeader(buf *bufio.Reader) (Headers, error) {
+	headers := make(map[string][]string)
+	var foundEmptyLine bool
+	for !foundEmptyLine {
+		l, err := readLine(buf)
+		if err != nil {
+			return nil, err
+		}
+
+		if string(l) == "" {
+			foundEmptyLine = true
+			break
+		}
+
+		k, v, ok := bytes.Cut(l, []byte(":"))
+		if !ok {
+			return nil, fmt.Errorf("invalid header format")
+		}
+
+		vs := strings.TrimLeft(string(v), " \t")
+
+		val, ok := headers[string(k)]
+		if !ok {
+			headers[string(k)] = []string{vs}
+		} else {
+			values := append(val, vs)
+			headers[string(k)] = values
+		}
+
+	}
+	return headers, nil
+}
+
+func readLine(buf *bufio.Reader) ([]byte, error) {
 	var fl []byte
 	for {
 		line, more, err := buf.ReadLine()
@@ -23,22 +94,15 @@ func ReadRequest(buf *bufio.Reader) (*Request, error) {
 			break
 		}
 	}
-
-	sl := strings.Split(string(fl), " ")
-	if len(sl) < 3 {
-		return nil, fmt.Errorf("invalid http data")
-	}
-	method, path, protoVersion := sl[0], sl[1], sl[2]
-
-	return &Request{
-		Method: method,
-		URI:    path,
-		Proto:  protoVersion,
-	}, nil
+	return fl, nil
 }
 
+type Headers map[string][]string
+
 type Request struct {
-	Method string
-	URI    string
-	Proto  string
+	Method  string
+	Headers Headers
+	URI     string
+	Proto   string
+	Body    io.ReadCloser
 }
--- server.go
+++ server.go
@@ -2,7 +2,9 @@ package http
 
 import (
 	"bufio"
+	"encoding/base64"
 	"fmt"
+	"io"
 	"log"
 	"net"
 )
@@ -16,12 +18,32 @@ func Listen(address string) error {
 			"\r\n"+
 			"%s", len(res), res)))
 	}
-	s := Server{Handler: handler}
+
+	postHandler := func(conn net.Conn, r *Request) {
+		data, err := io.ReadAll(r.Body)
+		if err != nil {
+			panic(err)
+		}
+		defer r.Body.Close()
+
+		b64 := base64.StdEncoding.EncodeToString(data)
+		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\n"+
+			"Content-Length: %d\r\n"+
+			"Content-Type: text/html\r\n"+
+			"\r\n"+
+			"%s", len(b64), b64)))
+	}
+
+	s := Server{
+		Handler:     handler,
+		PostHandler: postHandler,
+	}
 	return s.Listen(address)
 }
 
 type Server struct {
-	Handler func(conn net.Conn, r *Request)
+	Handler     func(conn net.Conn, r *Request)
+	PostHandler func(conn net.Conn, r *Request)
 }
 
 func (s Server) Listen(address string) error {
@@ -49,5 +71,10 @@ func (s Server) handleConn(conn net.Conn) {
 		log.Println(err)
 	}
 	defer conn.Close()
-	s.Handler(conn, request)
+	switch request.Method {
+	case "GET":
+		s.Handler(conn, request)
+	case "POST":
+		s.PostHandler(conn, request)
+	}
 }
